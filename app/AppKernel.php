1d948cf4dcee12bb3a0c383e537fb7bc8077ccc2
Alexandr Kalenyuk <kalenyuk.alex@yahoo.com>
2015-06-21 20:36:46 +0300

    add bundles for rest

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle()
+            new AppBundle\AppBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new Misd\GuzzleBundle\MisdGuzzleBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
