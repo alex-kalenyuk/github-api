638a0b56468b0c4fc9977e222af41730cf8ad9a5
Alexandr Kalenyuk <kalenyuk.alex@yahoo.com>
2015-06-22 01:50:25 +0300

    add pagination to v1

--- src/AppBundle/Controller/v1/ReposController.php
+++ src/AppBundle/Controller/v1/ReposController.php
@@ -3,16 +3,31 @@
 namespace AppBundle\Controller\v1;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReposController extends FOSRestController
 {
-    public function getReposAction()
+    /**
+     * Get the list of repositories
+     * @param ParamFetcher $paramFetcher
+     * @return array data
+     *
+     * @QueryParam(name="page", requirements="\d+", default="1", description="Page of the list.")
+     * @QueryParam(name="per_page", requirements="\d+", default="10")
+     */
+    public function getReposAction(ParamFetcher $paramFetcher)
     {
-        $view = $this->view(
-            $this->get('github_service')->getRepos(),
-            200
+        $repos = $this->get('github_service')->getRepos(
+            $paramFetcher->get('page'),
+            $paramFetcher->get('per_page')
         );
 
-        return $this->handleView($view);
+        return $this->handleView(
+            $this->view($repos, 200)
+        );
     }
 }
--- src/AppBundle/Services/GithubService.php
+++ src/AppBundle/Services/GithubService.php
@@ -6,16 +6,30 @@ use GuzzleHttp\Client;
 
 class GithubService
 {
+    /**
+     * @var Client
+     */
     public $client;
+    
 
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
-    public function getRepos()
+    /**
+     * Get all repositories from my account
+     *
+     * @param int $page
+     * @param int $perPage
+     * @return mixed
+     */
+    public function getRepos($page = 1, $perPage = 10)
     {
-        $response = $this->client->get('users/alex-kalenyuk/repos')->getBody()->getContents();
+        $response = $this->client
+            ->get("users/alex-kalenyuk/repos?page=" . $page . "&per_page=" . $perPage)
+            ->getBody()
+            ->getContents();
 
         return json_decode($response, true);
     }
